128e09015d38882304b30a9363c4805b4946b9ed
marion419 <daynvi@gmail.com>
2024-12-11 20:33:07 +0900

    [feat]러닝한 거리 구하는 기능

--- lib/ui/pages/running/running_view_model.dart
+++ lib/ui/pages/running/running_view_model.dart
@@ -1,8 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sprinchat_app/core/geolocator_helper.dart';
 
 class RunningViewModel extends AutoDisposeNotifier<List<double>> {
   @override
   List<double> build() {
     return [];
+  }
+
+  RunningViewModel({required this.startLat, required this.startLng});
+
+  double? distance;
+  double? speed;
+  double? calorie;
+  double? startTime;
+  double startLat;
+  double startLng;
+
+  Future<void> update() async {
+    final currentLocation = await GeolocatorHelper.getPosition();
+    if (currentLocation != null) {
+      final currentDistance = GeolocatorHelper.getDistance(
+        startLat,
+        startLng,
+        currentLocation.latitude,
+        currentLocation.longitude,
+      );
+    }
   }
 }
