396dfa2b47f06b0f2df996bba7b3fd57cda02abf
marion419 <daynvi@gmail.com>
2024-12-11 19:37:25 +0900

    [add]running_view_model.dart 추가

---
+++ lib/ui/pages/running/running_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RunningViewModel extends AutoDisposeNotifier<List<double>> {
+  @override
+  List<double> build() {
+    return [];
+  }
+}
