7ba540e9190b31ad932ae3afbd110d5bc31c86da
marion419 <daynvi@gmail.com>
2024-12-12 18:55:46 +0900

    [feat]RunningPage에 Consumer 적용

--- lib/ui/pages/running/running_page.dart
+++ lib/ui/pages/running/running_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/kakao_map.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_information.dart';
 
@@ -23,11 +25,24 @@ class RunningPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            RunningInformation(title: '달린 거리', value: distance),
-            RunningInformation(title: '평균 속력', value: speed),
-            RunningInformation(title: '소모한 칼로리', value: calorie),
-            SizedBox(height: 30),
-            KakaoMap(lat: startLat, lng: startLng)
+            Consumer(
+              builder: (context, ref, child) {
+                final viewModel = ref.read(runningViewModel.notifier);
+                viewModel.update(startTime, startLat, startLng);
+                final analysis = ref.watch(runningViewModel);
+                return Column(
+                  children: [
+                    RunningInformation(
+                        title: '달린 거리', value: analysis.distance!),
+                    RunningInformation(title: '평균 속력', value: analysis.speed!),
+                    RunningInformation(
+                        title: '소모한 칼로리', value: analysis.calorie!),
+                    SizedBox(height: 30),
+                  ],
+                );
+              },
+            ),
+            KakaoMap(lat: startLat, lng: startLng),
           ],
         ),
       ),
