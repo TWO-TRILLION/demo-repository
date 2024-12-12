e502cefd3e358f0896747d1f0dbfec5c9542b507
marion419 <daynvi@gmail.com>
2024-12-12 17:49:33 +0900

    [fix]RunningViewModel 변수 선언 위치 변경

--- lib/ui/pages/running/running_view_model.dart
+++ lib/ui/pages/running/running_view_model.dart
@@ -2,59 +2,45 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sprinchat_app/core/geolocator_helper.dart';
 
 class RunningState {
-  double? distance;
-  double? speed;
-  double? calorie;
-  DateTime time;
-
-  RunningState(
-    this.distance,
-    this.speed,
-    this.calorie,
-    this.time,
-  );
+  double? distance; // 달린 거리
+  double? speed; // 평균 속력
+  double? calorie; // 소모한 칼로리
+  int? hour; // 달린 시간(시)
+  int? minute; // 달린 시간(분)
 }
 
 class RunningViewModel extends Notifier<RunningState> {
   @override
   build() {
-    return RunningState(distance, speed, calorie, time);
+    return RunningState();
   }
 
-  RunningViewModel(
-      {required this.startLat,
-      required this.startLng,
-      required this.startTime});
-
-  DateTime startTime; // 러닝을 시작한 시간
-  double startLat; // 러닝을 시작한 지점의 위도
-  double startLng; // 러닝을 시작한 지점의 경도
-
-  double? distance;
-  double? speed;
-  double? calorie;
-  var time;
-
-  Future<void> update() async {
-    // 현재 위치 검색
-    final currentLocation = await GeolocatorHelper.getPosition();
+  Future<void> update(
+    DateTime startTime,
+    double startLat,
+    double startLng,
+  ) async {
+    final currentLocation = await GeolocatorHelper.getPosition(); // 현재 위치 좌표
+    var time = DateTime.now().difference(startTime); // 달린 시간
 
     if (currentLocation != null) {
-      // 달린 거리 계산
+      // distance : 달린 거리
       final distance = GeolocatorHelper.getDistance(
         startLat,
         startLng,
         currentLocation.latitude,
         currentLocation.longitude,
       );
-      // 달린 시간
-      time = DateTime.now().difference(startTime);
-      // 평균 속력(km/h)
-      speed = distance / time.inHours;
-      // 칼로리 소모 : 분당 4
-      calorie = time.inMinutes / 4;
-
-      state = RunningState(distance, speed, calorie, time);
+      state.hour = time.inHours; // hour : 달린 시간(시)
+      state.minute = DateTime.now().difference(startTime).inMinutes %
+          60; // minute : 달린 시간(분)
+      state.speed = distance / time.inHours; // 평균 속력(km/h)
+      state.calorie = time.inMinutes / 4; // calorie : 칼로리 소모 (분당 4kcal)
     }
   }
 }
+
+final runningViewModelProvider =
+    NotifierProvider.autoDispose<RunningViewModel, RunningState>(
+  () => RunningViewModel(),
+);
