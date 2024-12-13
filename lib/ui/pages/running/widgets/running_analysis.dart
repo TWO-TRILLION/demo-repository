d2e9659ced8a8affd2f52b5d123a8074d4c7b1fe
marion419 <daynvi@gmail.com>
2024-12-13 17:40:59 +0900

    [delete]원형 버튼 삭제

--- lib/ui/pages/running/widgets/running_analysis.dart
+++ lib/ui/pages/running/widgets/running_analysis.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
-import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_button.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_information.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/time_information.dart';
 
@@ -24,20 +23,65 @@ class RunningAnalysis extends StatelessWidget {
         final viewModel = ref.read(runningViewModel.notifier);
         viewModel.update(startTime, startLat, startLng);
         final analysis = ref.watch(runningViewModel);
-        return Column(
-          children: [
-            RunningButton(analysis),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RunningInformation('달린 거리', analysis.distance, 'km'),
-                RunningInformation('평균 속력', analysis.speed, 'km/h'),
-                RunningInformation('소모한 칼로리', analysis.calorie, 'kcal'),
-              ],
-            ),
-            SizedBox(height: 30),
-          ],
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '러닝',
+                    style: TextStyle(
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    '${DateTime.now().month}월 ${DateTime.now().day}일 ${DateTime.now().hour}시 ${DateTime.now().minute}분',
+                    style: TextStyle(
+                      color: Color(0xff979C9E),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                height: 1,
+                color: Color(0xff979C9E),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TimeInformation(
+                    value: analysis.hour,
+                    unit: 'h',
+                  ),
+                  SizedBox(width: 10),
+                  TimeInformation(
+                    value: analysis.minute,
+                    unit: 'm',
+                  ),
+                  SizedBox(width: 10),
+                  TimeInformation(
+                    value: analysis.second,
+                    unit: 's',
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RunningInformation('달린 거리', analysis.distance, 'km'),
+                  RunningInformation('평균 속력', analysis.speed, 'km/h'),
+                  RunningInformation('소모한 칼로리', analysis.calorie, 'kcal'),
+                ],
+              ),
+              SizedBox(height: 30),
+            ],
+          ),
         );
       },
     );
--- lib/ui/pages/running/widgets/running_button.dart
+++ lib/ui/pages/running/widgets/running_button.dart
@@ -9,58 +9,22 @@ class RunningButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Container(
-        width: double.infinity,
-        height: 300,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 40,
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '${DateTime.now().month}월 ${DateTime.now().day}일 ${DateTime.now().hour}시 ${DateTime.now().minute}분',
-              style: TextStyle(
-                color: Color(0xff979C9E),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 200,
-              height: 1,
-              color: Color(0xff979C9E),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TimeInformation(
-                  value: analysis.hour,
-                  unit: 'h',
-                ),
-                SizedBox(width: 10),
-                TimeInformation(
-                  value: analysis.minute,
-                  unit: 'm',
-                ),
-                SizedBox(width: 10),
-                TimeInformation(
-                  value: analysis.second,
-                  unit: 's',
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Container(
+      width: double.infinity,
+      height: 200,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 40,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
       ),
     );
   }
