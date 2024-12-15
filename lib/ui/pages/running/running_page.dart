f9cbea13e47fab2dc50ddcbdff34648ab36aeafa
marion419 <daynvi@gmail.com>
2024-12-15 15:47:22 +0900

    [feat]시간 카운트 올라가는 기능 추가

--- lib/ui/pages/running/running_page.dart
+++ lib/ui/pages/running/running_page.dart
@@ -1,22 +1,40 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sprinchat_app/core/geolocator_helper.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/kakao_map.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_analysis.dart';
 import 'package:flutter_sprinchat_app/ui/widgets/navigation_bar.dart';
+import 'package:geolocator/geolocator.dart';
 
-class RunningPage extends StatelessWidget {
+class RunningPage extends StatefulWidget {
   RunningPage({required this.startLat, required this.startLng});
 
+  double startLat;
+  double startLng;
+  Color boxColor = Color(0xff0070F0);
+  IconData icon = Icons.directions_walk;
+
+  @override
+  State<RunningPage> createState() => _RunningPageState();
+}
+
+class _RunningPageState extends State<RunningPage> {
   double distance = 0;
   double speed = 0;
   double calorie = 0;
   DateTime? time;
-  double startLat;
-  double startLng;
   DateTime startTime = DateTime.now();
   bool isRunning = false;
+  late Timer timer;
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +45,9 @@ class RunningPage extends StatelessWidget {
           child: Consumer(
             builder: (context, ref, child) {
               final viewModel = ref.read(runningViewModel.notifier);
+              viewModel.setLocation();
+              viewModel.update(startTime);
+              var analysis = ref.watch(runningViewModel);
               return Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -52,49 +73,81 @@ class RunningPage extends StatelessWidget {
                     ],
                   ),
                   SizedBox(height: 30),
+                  // Container(
+                  //   width: double.infinity,
+                  //   height: 10,
+                  //   color: widget.boxColor,
+                  //   child: GestureDetector(
+                  //     onTap: () {
+                  //       Timer.periodic(Duration(seconds: 1), (t) {
+                  //         widget.boxColor != Colors.yellow
+                  //             ? setState(() {
+                  //                 widget.boxColor = Colors.yellow;
+                  //               })
+                  //             : setState(() {
+                  //                 widget.boxColor = Colors.grey;
+                  //               });
+                  //       });
+                  //     },
+                  //   ),
+                  // ),
                   SizedBox(
                     height: 350,
                     child: Stack(
                       alignment: Alignment.topCenter,
                       children: [
-                        KakaoMap(lat: startLat, lng: startLng),
+                        KakaoMap(lat: widget.startLat, lng: widget.startLng),
                         Positioned(
                           bottom: 0,
-                          child: Container(
-                            width: 120,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              color: Color(0xff0070F0),
-                              shape: BoxShape.circle,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.7),
-                                  blurRadius: 5.0,
-                                  offset: Offset(0, 5),
-                                ),
-                              ],
-                            ),
-                            child: GestureDetector(
+                          child: GestureDetector(
                               onTap: () {
-                                if (isRunning) {
-                                  isRunning = false;
-                                } else {
+                                if (!isRunning) {
                                   isRunning = true;
-                                  Timer.periodic(const Duration(seconds: 1),
-                                      (t) {
-                                    viewModel.update(
-                                        startTime, startLat, startLng);
+                                  timer =
+                                      Timer.periodic(Duration(seconds: 1), (t) {
+                                    analysis = ref.watch(runningViewModel);
+                                    widget.icon == Icons.directions_walk
+                                        ? setState(() {
+                                            widget.icon = Icons.directions_run;
+                                          })
+                                        : setState(() {
+                                            widget.icon = Icons.directions_walk;
+                                          });
+                                  });
+                                } else {
+                                  isRunning = false;
+                                  timer.cancel();
+                                  setState(() {
+                                    widget.icon = Icons.directions_walk;
                                   });
                                 }
                               },
-                            ),
-                          ),
+                              child: Container(
+                                width: 120,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  color: widget.boxColor,
+                                  shape: BoxShape.circle,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.7),
+                                      blurRadius: 5.0,
+                                      offset: Offset(0, 5),
+                                    ),
+                                  ],
+                                ),
+                                child: Icon(
+                                  widget.icon,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                              )),
                         )
                       ],
                     ),
                   ),
                   SizedBox(height: 30),
-                  RunningAnalysis(),
+                  RunningAnalysis(analysis: analysis),
                 ],
               );
             },
--- lib/ui/pages/running/widgets/running_analysis.dart
+++ lib/ui/pages/running/widgets/running_analysis.dart
@@ -1,19 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_information.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/time_information.dart';
 
-class RunningAnalysis extends StatelessWidget {
-  const RunningAnalysis({
+class RunningAnalysis extends StatefulWidget {
+  RunningAnalysis({
     super.key,
+    required this.analysis,
   });
 
+  var analysis;
+
+  @override
+  State<RunningAnalysis> createState() => _RunningAnalysisState();
+}
+
+class _RunningAnalysisState extends State<RunningAnalysis> {
   @override
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, ref, child) {
-        final analysis = ref.watch(runningViewModel);
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 0),
           child: Column(
@@ -22,17 +31,17 @@ class RunningAnalysis extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TimeInformation(
-                    value: analysis.hour,
+                    value: widget.analysis.hour,
                     unit: 'h',
                   ),
                   SizedBox(width: 10),
                   TimeInformation(
-                    value: analysis.minute,
+                    value: widget.analysis.minute,
                     unit: 'm',
                   ),
                   SizedBox(width: 10),
                   TimeInformation(
-                    value: analysis.second,
+                    value: widget.analysis.second % 60,
                     unit: 's',
                   ),
                 ],
@@ -41,9 +50,10 @@ class RunningAnalysis extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  RunningInformation('달린 거리', analysis.distance, 'km'),
-                  RunningInformation('평균 속력', analysis.speed, 'km/h'),
-                  RunningInformation('소모한 칼로리', analysis.calorie, 'kcal'),
+                  RunningInformation('달린 거리', widget.analysis.distance, 'km'),
+                  RunningInformation('평균 속력', widget.analysis.speed, 'km/h'),
+                  RunningInformation(
+                      '소모한 칼로리', widget.analysis.calorie, 'kcal'),
                 ],
               ),
               SizedBox(height: 30),
