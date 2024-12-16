70c873cfc7036ad716667c6a9b8c234da9b605a8
marion419 <daynvi@gmail.com>
2024-12-16 23:59:22 +0900

    [feat]firebase에 러닝 결과 업데이트하는 함수

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -147,6 +148,18 @@ class UserRepository {
     } catch (e) {
       print('닉네임 업데이트 오류: $e');
       throw Exception('닉네임 업데이트 실패');
+    }
+  }
+
+  Future<void> updateRunningdata(String userId, RunningData data) async {
+    final json = jsonEncode(data);
+    try {
+      await firestore.collection('User').doc(userId).update({
+        'runningData': json,
+      });
+    } catch (e) {
+      print('러닝 기록 등록 오류: $e');
+      throw Exception('러닝 기록 등록 실패');
     }
   }
 
