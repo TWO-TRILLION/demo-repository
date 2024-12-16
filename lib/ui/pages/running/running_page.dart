7964e3d99347f85d23c59cca80780999ec5737fb
marion419 <daynvi@gmail.com>
2024-12-16 16:21:12 +0900

    [feat]러닝 시작/종료 버튼에 상태 추가

--- lib/ui/pages/running/running_page.dart
+++ lib/ui/pages/running/running_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/kakao_map.dart';
 import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_analysis.dart';
+import 'package:flutter_sprinchat_app/ui/pages/running/widgets/running_button.dart';
 import 'package:flutter_sprinchat_app/ui/widgets/navigation_bar.dart';
 
 class RunningPage extends StatefulWidget {
@@ -12,6 +13,7 @@ class RunningPage extends StatefulWidget {
 
   final double startLat; // 러닝을 시작한 좌표(위도)
   final double startLng; // 러닝을 시작한 좌표(경도)
+  bool isRunning = false;
 
   @override
   State<RunningPage> createState() => _RunningPageState();
@@ -29,6 +31,7 @@ class _RunningPageState extends State<RunningPage> {
           child: Consumer(
             builder: (context, ref, child) {
               // 뷰모델 선언
+              final runningButtonViewmodel = ref.read(buttonViewModel.notifier);
               return Column(
                 children: [
                   // 상단 '러닝' 문구
@@ -59,38 +62,28 @@ class _RunningPageState extends State<RunningPage> {
                         Positioned(
                           bottom: 0,
                           child: GestureDetector(
-                              onTap: () {
-                                // 러닝 시작
-                                if (!isRunning) {
-                                  isRunning = true;
-                                  final viewModel =
-                                      ref.read(runningViewModel.notifier);
-                                  viewModel.setLocation(); // 시작 위치 설정
-                                  viewModel.startRunning(DateTime
-                                      .now()); // 현재 시간으로 시작 시간 설정해서 러닝 시작
-                                } else {
-                                  isRunning = false;
-                                  ref
-                                      .read(runningViewModel.notifier)
-                                      .endRunning();
-                                }
-                              },
-                              // 버튼 디자인
-                              child: Container(
-                                width: 120,
-                                height: 120,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff0070F0),
-                                  shape: BoxShape.circle,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.7),
-                                      blurRadius: 5.0,
-                                      offset: Offset(0, 5),
-                                    ),
-                                  ],
-                                ),
-                              )),
+                            onTap: () {
+                              // 러닝 시작
+                              if (!isRunning) {
+                                isRunning = true;
+                                runningButtonViewmodel.startRunning();
+                                final viewModel =
+                                    ref.read(runningViewModel.notifier);
+                                viewModel.setLocation(); // 시작 위치 설정
+                                viewModel.startRunning(
+                                    DateTime.now()); // 현재 시간으로 시작 시간 설정해서 러닝 시작
+                              } else {
+                                isRunning = false;
+                                runningButtonViewmodel.stopRunning();
+                                ref
+                                    .read(runningViewModel.notifier)
+                                    .endRunning();
+                              }
+                            },
+                            // 버튼 디자인
+                            child: RunningButton(
+                                ref.watch(buttonViewModel).isRunning),
+                          ),
                         )
                       ],
                     ),
