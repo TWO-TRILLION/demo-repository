d64287af30341ff6fb0e99c066ac6e705859bc99
marion419 <daynvi@gmail.com>
2024-12-16 23:23:41 +0900

    [feat]kakaomap viewmodel 설계

--- lib/ui/pages/running/widgets/kakaomap.dart
+++ lib/ui/pages/running/widgets/kakaomap.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sprinchat_app/ui/pages/running/running_view_model.dart';
+import 'package:flutter_sprinchat_app/ui/pages/running/widgets/kakaomap_view_model.dart';
 import 'package:kakaomap_webview/kakaomap_webview.dart';
 
-class KakaoMap extends StatelessWidget {
-  const KakaoMap({
+class KakaoMap extends StatefulWidget {
+  KakaoMap({
     super.key,
     required this.lat,
     required this.lng,
   });
 
-  final double lat;
-  final double lng;
+  double lat;
+  double lng;
 
   @override
+  State<KakaoMap> createState() => _KakaoMapState();
+}
+
+class _KakaoMapState extends State<KakaoMap> {
+  @override
   Widget build(BuildContext context) {
-    return KakaoMapView(
-      width: double.infinity,
-      height: 300,
-      kakaoMapKey: '08eebe69029ef27e6209ec2d97b79d29',
-      lat: lat,
-      lng: lng,
+    return Consumer(
+      builder: (context, ref, child) {
+        ref.read(kakaomapViewModel.notifier).startMap();
+        return ref.watch(kakaomapViewModel).mapView!;
+        // return KakaoMapView(
+        //   width: double.infinity,
+        //   height: 300,
+        //   kakaoMapKey: '08eebe69029ef27e6209ec2d97b79d29',
+        //   lat: widget.lat,
+        //   lng: widget.lng,
+        // );
+      },
     );
   }
 }
