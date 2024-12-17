86aca5f631745770a46bb4e539fba0acb949ebf0
marion419 <daynvi@gmail.com>
2024-12-17 11:37:06 +0900

    [refactor]button_view_model 주석 추가

--- lib/ui/pages/running/widgets/button_view_model.dart
+++ lib/ui/pages/running/widgets/button_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// 러닝 시작/러닝 종료 버튼 UI를 바꾸기 위해 bool값을 전달하는 뷰모델
 class Button {
   bool isRunning = false;
   Button(this.isRunning);
